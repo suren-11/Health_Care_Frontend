bcf3b4946301a3038b09ef9a908c7c7f9513e7ba
Mufeel Ishad <muhammedmufeel876@gmail.com>
2024-03-16 00:33:06 +0530

    SplashScreen completed
    completed the SplashScreen with animation and full of responsive

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <StatusBar style="light" />
+     
       <Navigation/>
    
     </View>
--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -4,13 +4,15 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screen/LoginScreen';
+import SplashScreen from '../screen/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-        <StatusBar backgroundColor='white' barStyle="dark-content" />
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='LoginScreen'>
+        <StatusBar  backgroundColor='white' barStyle='dark-content' />
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='SplashScreen'>
+            <Stack.Screen name='SplashScreen' component={SplashScreen}/>
             <Stack.Screen name ='LoginScreen' component = {LoginScreen}/>
         </Stack.Navigator>
     </NavigationContainer>
